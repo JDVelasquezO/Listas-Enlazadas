a16f1d53295923b5398261e88e0567bc502202d8
JDVelasquezO <jdvela158@gmail.com>
2019-10-16 04:26:53 -0600

    Method of obtaining created

--- src/app/App.java
+++ src/app/App.java
@@ -1,8 +1,11 @@
 package app;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         List l = new List();
         l.addStart("Hola");
+        l.addStart("Adios");
+        Object object = l.getValue(1);
+        System.out.println(object);
     }
 }
--- src/app/List.java
+++ src/app/List.java
@@ -11,6 +11,7 @@ public class List {
         start = null;
     }
 
+    // Metodos de incersión:
     public void addStart (Object value) {
         Node node = new Node(value);
         node.next = start;
@@ -28,6 +29,26 @@ public class List {
             }
 
             pointer.next = node;
+        }
+    }
+
+    // Método de obtención:
+    public Object getValue (int n) {
+        if (start == null) {
+            return null;
+        } else {
+            Node pointer = start;
+            int counter = 0;
+            while (counter < n && pointer.next != null) {
+                pointer = pointer.next;
+                counter++;
+            }
+
+            if (counter != n) {
+                return null;
+            } else {
+                return pointer.value;
+            }
         }
     }
 }
