00074fd4ceb1fe68246ba59ce68b36aa43ef59a4
JDVelasquezO <jdvela158@gmail.com>
2019-10-16 05:13:54 -0600

    First Implementation

--- src/app/App.java
+++ src/app/App.java
@@ -3,11 +3,18 @@ package app;
 public class App {
     public static void main(String[] args) {
         List l = new List();
+        
         l.addStart("Java");
         l.addFinish("Python");
         l.addFinish("Vue");
+        l.addFinish("Angular");
+        
+        l.deleteFinish();
+        l.deleteStart();
         l.deleteForCounter(0);
-        Object object = l.getValue(1);
+        
+        Object object = l.getValue(0);
+        // int length = l.getLength();
         System.out.println(object);
     }
 }
